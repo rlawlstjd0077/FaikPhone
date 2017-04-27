3a9ec413f358463a5714931a947fa9cc9175b073
boxfox <rlatjdfo112@naver.com>
2017-04-27 11:32:13 +0900

    Notification Builder Finish

---
+++ Client/app/src/main/java/com/faikphone/client/NotificationBuilder.java
@@ -0,0 +1,43 @@
+package com.faker.notificationapplication;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.NotificationCompat;
+
+/**
+ * Created by boxfox on 2017-04-20.
+ */
+
+public class NotificationBuilder {
+    public static final int TYPE_SMS = 1;
+    public static final int TYPE_MISSED_CALL = 2;
+
+    public static void missedCall(Context context, String name, String phone){
+        build(context, R.drawable.missed_call, "부재중 전화", name+" "+phone );
+    }
+
+    public static void sms(Context context, String name, String content){
+        build(context, R.drawable.sms, name, content );
+    }
+
+    public static void build(Context context, int icon, String title, String content) {
+        NotificationManager nm = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, MainActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+
+        Notification.Builder mBuilder = new Notification.Builder(context);
+        mBuilder.setSmallIcon(icon);
+        mBuilder.setTicker("TestAAA");
+        mBuilder.setWhen(System.currentTimeMillis());
+        mBuilder.setNumber(10);
+        mBuilder.setContentTitle(title);;
+        mBuilder.setContentText(content);
+        mBuilder.setDefaults(Notification.DEFAULT_SOUND | Notification.DEFAULT_VIBRATE);
+        mBuilder.setContentIntent(pendingIntent);
+        mBuilder.setAutoCancel(true);
+
+        nm.notify(111, mBuilder.build());
+    }
+}
