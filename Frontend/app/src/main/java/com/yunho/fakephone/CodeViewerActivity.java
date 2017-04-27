9d57e39a37909fbc290ed6911c87373ae880ce31
PRKM <dudgns2486@gmail.com>
2017-04-27 14:40:49 +0900

    Added Frontend Section

---
+++ Frontend/app/src/main/java/com/yunho/fakephone/CodeViewerActivity.java
@@ -0,0 +1,13 @@
+package com.yunho.fakephone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CodeViewerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_code_viewer);
+    }
+}
