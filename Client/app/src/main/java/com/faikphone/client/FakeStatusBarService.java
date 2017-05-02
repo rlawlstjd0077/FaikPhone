435b73ffb756ffd4d495b81457adee8676c9da87
BeINone <sbeinone@gmail.com>
2017-05-02 10:03:01 +0900

    Changed settings menu icon, loaded setting info from SharedPreferences

--- Client/app/src/main/java/com/faikphone/client/FakeStatusBarService.java
+++ Client/app/src/main/java/com/faikphone/client/FakeStatusBarService.java
@@ -30,7 +30,7 @@ public class FakeStatusBarService extends Service {
 
     private AppPreferences mAppPrefs;
     private WindowManager mWindowManager;
-    private View mView;
+    private View mFakeStatusBar;
     private TextView mTimeTV;
 
     @Override
@@ -39,56 +39,19 @@ public class FakeStatusBarService extends Service {
 
         mAppPrefs = new AppPreferences(this);
 
-        if (!mAppPrefs.isFakeStatusBarMode()) {
-            mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
-
-            LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
-            mView = layoutInflater.inflate(R.layout.view_fake_status_bar, null);
-
-            mTimeTV = (TextView) mView.findViewById(R.id.tv_time);
-            setCurrentTime();
-
-            WindowManager.LayoutParams params = new WindowManager.LayoutParams(
-                    WindowManager.LayoutParams.MATCH_PARENT,
-                    (int) DensityConverter.dpToPx(this, 28),
-                    // Allows the view to be on top of the StatusBar
-                    WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
-                    // Keeps the button presses from going to the background window
-                    WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
-                            // pass touch event to real status bar behind this view
-                            WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
-                            // Enables the notification to receive touch events
-                            WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
-                            // Draws over status bar
-                            WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
-                    PixelFormat.TRANSLUCENT);
-            params.gravity = Gravity.TOP | Gravity.CENTER;
-
-            mView.setOnTouchListener(new View.OnTouchListener() {
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    return false;
-                }
-            });
-
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if (Settings.canDrawOverlays(this)) {
-                    mWindowManager.addView(mView, params);
-                } else {
-                    Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show();
-                }
-            }
-
-            registerReceiver(mTimeChangedReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+        if (mAppPrefs.isFakeStatusBarMode()) {
+            showFakeStatusBar();
         } else {
-            stopSelf();
+            hideFakeStatusBar();
         }
+
+        registerReceiver(mPreferenceChangedReceiver, new IntentFilter(getString(R.string.preferences_changed_broadcast)));
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        if (mView != null) mWindowManager.removeView(mView);
+        if (mFakeStatusBar != null) mWindowManager.removeViewImmediate(mFakeStatusBar);
         if (mTimeChangedReceiver != null) {
             try {
                 unregisterReceiver(mTimeChangedReceiver);
@@ -104,6 +67,53 @@ public class FakeStatusBarService extends Service {
         return null;
     }
 
+    private void showFakeStatusBar() {
+        mWindowManager = (WindowManager) getSystemService(WINDOW_SERVICE);
+
+        LayoutInflater layoutInflater = (LayoutInflater) getSystemService(LAYOUT_INFLATER_SERVICE);
+        mFakeStatusBar = layoutInflater.inflate(R.layout.view_fake_status_bar, null);
+
+        mTimeTV = (TextView) mFakeStatusBar.findViewById(R.id.tv_time);
+        setCurrentTime();
+
+        WindowManager.LayoutParams params = new WindowManager.LayoutParams(
+                WindowManager.LayoutParams.MATCH_PARENT,
+                (int) DensityConverter.dpToPx(this, 28),
+                // Allows the view to be on top of the StatusBar
+                WindowManager.LayoutParams.TYPE_SYSTEM_ERROR,
+                // Keeps the button presses from going to the background window
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE |
+                        // pass touch event to real status bar behind this view
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE |
+                        // Enables the notification to receive touch events
+                        WindowManager.LayoutParams.FLAG_NOT_TOUCH_MODAL |
+                        // Draws over status bar
+                        WindowManager.LayoutParams.FLAG_LAYOUT_IN_SCREEN,
+                PixelFormat.TRANSLUCENT);
+        params.gravity = Gravity.TOP | Gravity.CENTER;
+
+        mFakeStatusBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                return false;
+            }
+        });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                mWindowManager.addView(mFakeStatusBar, params);
+            } else {
+                Toast.makeText(this, "권한이 필요합니다.", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        registerReceiver(mTimeChangedReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+    }
+
+    private void hideFakeStatusBar() {
+        if (mFakeStatusBar != null) mWindowManager.removeViewImmediate(mFakeStatusBar);
+    }
+
     private void setCurrentTime() {
         SimpleDateFormat sdf = new SimpleDateFormat("h:mm", Locale.KOREA);
         String currentTime = sdf.format(new Date());
@@ -116,6 +126,19 @@ public class FakeStatusBarService extends Service {
             String action = intent.getAction();
             if (action.equals(Intent.ACTION_TIME_TICK)) {
                 setCurrentTime();
+            }
+        }
+    };
+
+    private BroadcastReceiver mPreferenceChangedReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(getString(R.string.preferences_changed_broadcast))) {
+                if (mAppPrefs.isFakeStatusBarMode()) {
+                    showFakeStatusBar();
+                } else {
+                    hideFakeStatusBar();
+                }
             }
         }
     };
