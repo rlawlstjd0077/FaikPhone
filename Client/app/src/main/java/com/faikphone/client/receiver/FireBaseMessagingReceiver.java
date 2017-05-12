98d10d1adbb80005758ace44221a8b3560a605a0
JinSeong <rlawlstjd077@gmail.com>
2017-05-12 14:18:54 +0900

    remove json

--- Client/app/src/main/java/com/faikphone/client/receiver/FireBaseMessagingReceiver.java
+++ Client/app/src/main/java/com/faikphone/client/receiver/FireBaseMessagingReceiver.java
@@ -1,12 +1,14 @@
 package com.faikphone.client.receiver;
 
 import android.content.Intent;
+import android.util.Log;
 
 import com.faikphone.client.utils.AppPreferences;
 import com.faikphone.client.activity.CallActivity;
 import com.faikphone.client.application.FaikPhoneApplication;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -14,29 +16,28 @@ import org.json.JSONObject;
  * Created by dsm_025 on 2017-04-13.
  */
 
-public class FireBaseMessagingReceiver extends FirebaseMessagingService{
+public class FireBaseMessagingReceiver extends FirebaseMessagingService {
     AppPreferences appPreferences = FaikPhoneApplication.getAppPreferences();
+
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         Log.d("onMessageReceived: ", remoteMessage.getData().get("message"));
-        try {
-            if(!appPreferences.getPhoneMode()) {        // Fake Phone 일 경우
-                handleMessage(remoteMessage.getData().get("message"));
+        if (!appPreferences.getPhoneMode()) {        // Fake Phone 일 경우
+            switch (remoteMessage.getData().get("type")) {
+                case "call":
+                    Intent intent = new Intent(this, CallActivity.class);
+                    intent.putExtra("name", remoteMessage.getData().get("name"));
+                    intent.putExtra("numver", remoteMessage.getData().get("number"));
+                    startActivity(intent);
+                    break;
+                case "message":
+
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
     }
 
     public void handleMessage(String message) throws JSONException {
         JSONObject object = new JSONObject(message);
-        switch (object.getString("type")){
-            case "call" :
-                Intent intent = new Intent(this, CallActivity.class);
-                intent.putExtra("name", object.getString("name"));
-                intent.putExtra("numver", object.getString("number"));
-                startActivity(intent);
-                break;
-        }
+
     }
 }
