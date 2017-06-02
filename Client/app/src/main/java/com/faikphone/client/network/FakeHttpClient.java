1e81390b37668e2dfabb80d4124553ae16c373b3
JinSeong <rlawlstjd077@gmail.com>
2017-06-02 10:20:47 +0900

    Remove resetAll in FakeHttpClient

--- Client/app/src/main/java/com/faikphone/client/network/FakeHttpClient.java
+++ Client/app/src/main/java/com/faikphone/client/network/FakeHttpClient.java
@@ -42,12 +42,6 @@ public class FakeHttpClient extends HttpClient{
     @Override
     public void doSendMessage(JSONObject msg, String token) throws JSONException {
         HttpUrl.Builder urlBuilder = createBuilder("send_message", token);
-//        Iterator<String> keys = msg.keys();
-//        while (keys.hasNext()) {
-//            String key = keys.next();
-//            String value = msg.getString(key);
-//            urlBuilder.addQueryParameter(key, value);
-//        }
         final Request request = getRequest(urlBuilder, msg.toString());
         doRequest(request, true);
     }
@@ -58,9 +52,6 @@ public class FakeHttpClient extends HttpClient{
 
     @Override
     public void doResetAll(String token) {
-        HttpUrl.Builder urlBuilder = createBuilder("reset_all", token);
-        final Request request = getRequest(urlBuilder, "");
-        doRequest(request, true);
     }
 
     @Override
